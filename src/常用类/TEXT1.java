1afef38bcadc935e271638945555b7c5fa93bfac
huangdingluan <316943766@qq.com>
2019-04-15 22:23:12 +0800

    1

---
+++ src/常用类/TEXT1.java
@@ -0,0 +1,23 @@
+package 常用类;
+
+public class TEXT1 {
+
+	private static String str = "this is a test of java";
+
+	public static void main(String[] args) {
+	test1();
+	}
+
+	public static void test1(){
+
+	int count = 0;
+	for (int i = 0; i < str.length(); i++) {
+	if('s'==str.charAt(i)){
+	count++;
+	}
+	}
+
+	System.out.println("s出现的次数:"+count);
+
+	}
+}
