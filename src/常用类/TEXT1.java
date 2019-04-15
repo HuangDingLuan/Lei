30576abbffb302c96d2bab9c9e37818db7b70811
huangdingluan <316943766@qq.com>
2019-04-15 22:48:48 +0800

    3

--- src/常用类/TEXT1.java
+++ src/常用类/TEXT1.java
@@ -1,5 +1,7 @@
 package 常用类;
 
+import java.util.Arrays;
+
 public class TEXT1 {
 
 	private static String str = "this is a test of java";
@@ -7,6 +9,7 @@ public class TEXT1 {
 	public static void main(String[] args) {
 	test1();
 	test2();
+	test3();
 	}
 
 	public static void test1(){
@@ -25,5 +28,14 @@ public class TEXT1 {
 		int start = str.indexOf("test");
 		String s = str.substring(start, start+5);
 		System.out.println("取出"+s);
+	}
+	public static void test3() {
+		char[] ch = new char[str.length()];
+		for(int i=0; i<str.length();i++) {
+			ch[i]=str.charAt(i);
+		}
+		System.out.println(Arrays.toString(ch));
+		char[] ch2 = str.toCharArray();
+		System.out.println(Arrays.toString(ch2));
 	}
 }
