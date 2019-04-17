533ea960fe1ce75a4f3cbe076d7481f4cec65db5
huangdingluan <316943766@qq.com>
2019-04-17 23:16:44 +0800

    9

--- src/常用类/TEXT2.java
+++ src/常用类/TEXT2.java
@@ -9,6 +9,15 @@ public class TEXT2 {
 		//String对象与StringBuffer对象互相转换
 		sb.append("上山打老虎");
 		System.out.println(sb);
+		//添加
+		sb.delete(0,9);
+		System.out.println(sb);
+		//删除
+		sb.insert(0, "我");
+		System.out.println(sb);
+		//插入
+		System.out.println(sb.reverse());
+		//反转
 		// TODO Auto-generated method stub
 
 	}
