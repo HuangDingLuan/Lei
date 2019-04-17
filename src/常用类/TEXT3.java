fb0c5be7abd4348a2654a540927538036dd2b4b6
huangdingluan <316943766@qq.com>
2019-04-17 23:39:24 +0800

    9

---
+++ src/常用类/TEXT3.java
@@ -0,0 +1,19 @@
+package 常用类;
+
+public class TEXT3 {
+
+	public static void main(String[] args) {
+		int s=3169;
+		Integer a=new Integer(1348);
+		String str=new String("3766");
+		
+		Integer b=new Integer(s);
+		//基本数据类型转化为包装类
+		
+		int s1=a.intValue();
+		//包装类转化基本数据类型
+		
+	}
+		// TODO Auto-generated method stub
+	}
+
